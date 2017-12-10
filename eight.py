3b5b11871087082e2dbc79ece496460ed315fedd
Steve Geluso <stevegeluso@gmail.com>
2017-12-09 20:28:28 -0800

    eight pt1

--- eight.py
+++ eight.py
@@ -12,7 +12,7 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)"""
 
-i1 = """navfz (187) -> jviwcde, wfwor, vpfabxa
+i2 = """navfz (187) -> jviwcde, wfwor, vpfabxa
 adtfnx (74)
 endsg (33)
 wcswjri (228) -> jcnfvhv, yprbzn
@@ -1881,24 +1881,82 @@ lxuwkr (16)
 zczzj (89)
 xszevpx (8)"""
 
+from collections import defaultdict
+
+weights = {}
 names = []
+holding = defaultdict(list)
 being_held = []
-for line in i1.split("\n"):
-    print(line)
+for line in i2.split("\n"):
+    #print(line)
     cells = line.split(" ")
-    name, weight = cells[0],  cells[1][1:-1]
-    print("name:", name)
+    name, weight = cells[0],  int(cells[1][1:-1])
+    #print("name:", name, weight)
+    weights[name] = weight
     names.append(name)
     if len(cells) > 2:
         held_cells = line.split(" -> ")[1]
         if ", " in held_cells:
             held_cells = held_cells.split(", ")
-            for name in held_cells:
-                print("being held", name)
-                being_held.append(name)
+            for held in held_cells:
+                #print("being held", name)
+                holding[name].append(held)
+                being_held.append(held)
         else:
             being_held.append(held_cells)
 print(len(names), len(being_held))
+
+bottom = None
 for name in names:
     if name not in being_held:
         print("bottom", name)
+        bottom = name
+
+def weigh(name, level=0):
+    current = weights[name]
+    nodes = holding[name]
+    last_held = None
+    wacky = False
+    for node in nodes:
+        pounds = weigh(node, level + 1)
+        current += pounds
+        if last_held is None:
+            last_held = pounds + weights[node]
+        elif pounds != last_held:
+            wacky = True
+    if wacky:
+        print()
+        print("bottom weight:", current, name)
+        print(name, holding[name])
+        for node in holding[name]:
+            print("wack at", level, "from", name, "->", node, weights[node], weigh(node, level + 1))
+
+    return current
+
+# not 254 from 
+# wack at 2 from vfjnsd -> quempkz 1225 2062
+# wack at 2 from vfjnsd -> xvlxy 77 2062
+# wack at 2 from vfjnsd -> tulwp 264 2070
+
+# not 72662 from
+# wack at 0 from bpvhwhh -> xnmjpa 72670 115030
+# wack at 0 from bpvhwhh -> rilgrr 75761 115022
+# wack at 0 from bpvhwhh -> fvrrpo 34174 115022
+# wack at 0 from bpvhwhh -> zcmlgn 75203 115022
+total = weigh(bottom)
+print(total)
+    
+i1 = """pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)"""
+
